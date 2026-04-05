be4bb8a70e52f537be8c17032669c49dfe42a609
Akiicodes <akshit.sinha1356@gmail.com>
2026-04-05 22:50:37 +0530

    added music feature 🎵💖

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const text = "Babyyy, this one month with you has been the happiest time of my l
 function showLoveLetter() {
   document.getElementById("letterBox").style.display = "block";
   let i = 0;
+  document.getElementById("bgMusic").play();
 
   function typeWriter() {
     if (i < text.length) {
@@ -28,3 +29,19 @@ setInterval(() => {
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   document.getElementById("timer").innerText = days + " days with my babyyy 💖";
 }, 1000);
+let isPlaying = false;
+
+function toggleMusic() {
+  const music = document.getElementById("bgMusic");
+  const btn = document.getElementById("musicBtn");
+
+  if (!isPlaying) {
+    music.play();
+    btn.innerText = "Pause Song ⏸️";
+    isPlaying = true;
+  } else {
+    music.pause();
+    btn.innerText = "Play Our Song 🎵";
+    isPlaying = false;
+  }
+}

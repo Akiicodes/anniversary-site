9176fdedee9cdb5a3aae9bd8113a3d6a67c91427
Akiicodes <akshit.sinha1356@gmail.com>
2026-04-05 21:54:32 +0530

    update: improved UI + added photos 💖

---
+++ script.js
@@ -0,0 +1,30 @@
+function playMusic() {
+  document.getElementById("bgMusic").play();
+}
+
+const text = "Babyyy, this one month with you has been the happiest time of my life. You make everything feel better, and I genuinely can't imagine my days without you anymore. I love you so much ❤️";
+
+function showLoveLetter() {
+  document.getElementById("letterBox").style.display = "block";
+  let i = 0;
+
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("letterText").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 40);
+    }
+  }
+
+  document.getElementById("letterText").innerHTML = "";
+  typeWriter();
+}
+
+const startDate = new Date("2026-03-05");
+
+setInterval(() => {
+  const now = new Date();
+  const diff = now - startDate;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  document.getElementById("timer").innerText = days + " days with my babyyy 💖";
+}, 1000);
